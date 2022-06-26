dae1f36cbe462f0e9cd0249b8a3f69ff0e7b2615
sem <sebastian.maischl@ivfp.de>
2022-06-26 22:26:49 +0200

    Init

---
+++ app.py
@@ -0,0 +1,22 @@
+from defer import return_value
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+
+    text = "provider"
+
+    returnValue = text, 200
+
+    if os.environ.get("fail", "false") == 'true':
+        returnValue = "failed", 500
+
+    return returnValue
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
